3a7141d25dd4fd4123a3acb58c70fb73c9a6701a
Victor <victor.alcazar.alcazar@gmail.com>
2017-01-23 00:42:09 +0100

    Add images and json with astronomers data

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -4,6 +4,5 @@
 angular.module("astronomersModule")
     .controller('AstronomersListCtrl', ['$scope', '$log', function ($scope, $log) {
         $log.info("Initialising astronomersList");
-        $scope.elements = [{id:0,photo: '', name:'Eratosthenes of Cyrene',birth:'276 BC', description: 'Eratosthenes of Cyrene was a Greek mathematician, geographer, poet, astronomer, and music theorist. He was a man of learning, becoming the chief librarian at the Library of Alexandria. He invented the discipline of geography, including the terminology used today.'},
-            {id:1,photo: '', name:'Rob',birth:'1981', description: 'lol 2'}]
+        $scope.elements = {}
     }]);
