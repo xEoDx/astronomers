893ea28fc427f9fcb4712e497b32083d975d3fac
Victor <victor.alcazar.alcazar@gmail.com>
2017-01-24 00:52:18 +0100

    Update style and carousel timeline

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -2,7 +2,13 @@
  * Created by Victor on 1/22/2017.
  */
 angular.module("astronomersModule")
-    .controller('AstronomersListCtrl', ['$scope', '$log', function ($scope, $log) {
+    .controller('AstronomersListCtrl', ['$scope', '$http', '$log', function ($scope, $http, $log) {
         $log.info("Initialising astronomersList");
-        $scope.elements = {}
+        $scope.elements = [];
+
+        $http.get('resources/data/astronomers.json').then(function(data) {
+            $scope.elements = data.data;
+            $log.info("Astronomers list: ",$scope.elements);
+        });
+
     }]);
--- src/main/webapp/views/carousel/carousel.js
+++ src/main/webapp/views/carousel/carousel.js
@@ -12,7 +12,46 @@ angular.module("carousel", [])
             controller: 'carouselController'
         };
     })
-    .controller("carouselController", ['$scope', '$log', function ($scope, $log) {
+    .filter('year', function() {
+        return function(input, optional1, optional2) {
+            var parsedDate = 0;
+            var splittedDate = input.split(" ");
+            if(splittedDate.length == 3) {
+                parsedDate = splittedDate[2];
+            } else if(splittedDate.length == 2) {
+                parsedDate = splittedDate[0];
+            } else {
+                console.log("Error obtaining parsed date: ", parsedDate);
+            }
+            return parsedDate;
+
+        }
+
+    })
+    .controller("carouselController", ['$scope', '$log', '$interval', function ($scope, $log, $interval) {
         $log.info("Initializing carousel... with elements: ", $scope.elements);
         $scope.value = 0;
+        $scope.isPlaying = false;
+
+        $scope.dotClick = function(index) {
+            $log.info("User clicked on dot: ",index);
+            $scope.value=index;
+        };
+
+        $scope.carouselInterval = null;
+
+        $scope.play = function () {
+            $scope.isPlaying = true;
+            $scope.carouselInterval = $interval(function () {
+                $scope.value = ($scope.value + 1) % $scope.elements.length;
+            }, 10000);
+
+        };
+
+        $scope.stop = function () {
+            $scope.isPlaying = false;
+            $interval.cancel($scope.carouselInterval);
+        };
+
+
     }]);
